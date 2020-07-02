e64477108130258ec3027f0957d61a67877aedd1
cc home <cc258520@gmail.com>
2020-07-03 00:47:20 +0800

    feat: react 16 pages

---
+++ react16/src/pages/about/about.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="pages home" id="home">About</div>
+  );
+};
+
+export default About;
---
+++ react16/src/pages/home/home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="pages home" id="home">Home</div>
+  );
+};
+
+export default Home;
